448319b5ed7f7cb97d85b8318d92ca6bff0ae093
Xiang Liu <liuxiang625@gmail.com>
2013-09-27 17:50:33 -0700

    Start of admin eval input page

---
+++ SummitApp2013/WebFolder/AdminInput.waPage/scripts/index.js
@@ -0,0 +1,26 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var textField2 = {};	// @textField
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	textField2.keyup = function textField2_keyup (event)// @startlock
+	{// @endlock
+		var searchPhrase = $$('textField2').getValue();
+		sources.session.query("title begin :1 AND isActivity == null", searchPhrase);
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("textField2", "keyup", textField2.keyup, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
