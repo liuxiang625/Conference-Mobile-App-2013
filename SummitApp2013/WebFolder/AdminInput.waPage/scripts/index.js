63b315ade31861611de1aa31c8f9c49c443aa035
Xiang Liu <liuxiang625@gmail.com>
2013-10-02 11:14:42 -0700

    Admin page implemented, need attendee lookup

--- SummitApp2013/WebFolder/AdminInput.waPage/scripts/index.js
+++ SummitApp2013/WebFolder/AdminInput.waPage/scripts/index.js
@@ -2,10 +2,26 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var imageButton1 = {};	// @buttonImage
+	var button2 = {};	// @button
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	imageButton1.click = function imageButton1_click (event)// @startlock
+	{// @endlock
+		source.answer.addNewElement();
+		$$('container2').show();
+	};// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		sources.answer.survey.set(sources.survey);
+		sources.answer.save();
+		$$('container2').hide();
+		console.log("answer saved");
+	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
@@ -13,6 +29,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("imageButton1", "click", imageButton1.click, "WAF");
+	WAF.addListener("button2", "click", button2.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
