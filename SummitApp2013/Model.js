ae53f8b38ecf203b555bc0c55fdb30af695bfe9e
Xiang Liu <liuxiang625@gmail.com>
2013-10-02 12:28:06 -0700

    attendee idtifying not finished on admininput page, start phone page

--- SummitApp2013/Model.js
+++ SummitApp2013/Model.js
@@ -1,4 +1,25 @@
 ﻿
 guidedModel =// @startlock
 {
+	Attendee :
+	{
+		events :
+		{
+			onInit:function()
+			{// @endlock
+				this.ID = generateUUID();
+				debugger;
+			}// @startlock
+		}
+	},
+	Answer :
+	{
+		events :
+		{
+			onSave:function()
+			{// @endlock
+				//console.log(this.attendeeName);
+			}// @startlock
+		}
+	}
 };// @endlock
--- SummitApp2013/WebFolder/AdminInput.waPage/scripts/index.js
+++ SummitApp2013/WebFolder/AdminInput.waPage/scripts/index.js
@@ -1,6 +1,6 @@
 ﻿
 WAF.onAfterInit = function onAfterInit() {// @lock
-
+	
 // @region namespaceDeclaration// @startlock
 	var imageButton1 = {};	// @buttonImage
 	var button2 = {};	// @button
@@ -11,21 +11,51 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	imageButton1.click = function imageButton1_click (event)// @startlock
 	{// @endlock
-		source.answer.addNewElement();
-		$$('container2').show();
+		source.answers.addNewElement();
+		
+		//Init radio button group valuee;
+		source.answers.answer1 = 5;
+		source.answers.answer2 = 5;
+		source.answers.answer3 = 5;
+		source.answers.answer4 = 5;
+		source.answers.answer5 = 5;
+		$$('radioGroup1').setValue(5);
+		$$('radioGroup3').setValue(5);
+		$$('radioGroup4').setValue(5);
+		$$('radioGroup5').setValue(5);
+		$$('radioGroup6').setValue(5);
+		$$('button1').show();
+		$$('button2').show();
 	};// @lock
 
 	button2.click = function button2_click (event)// @startlock
 	{// @endlock
-		sources.answer.survey.set(sources.survey);
-		sources.answer.save();
-		$$('container2').hide();
+		if(attendeeObj.email)
+		sources.attendee.query("email = :1", attendeeObj.email,{
+        onSuccess: function(){
+				if(sources.attendee.length == 0 ) {
+					debugger;
+					sources.attendee.addNewElement();
+					sources.attendee.fullName = attendeeObj.name;
+					sources.attendee.email = attendeeObj.email;
+					sources.attendee.save();
+				}
+				sources.answers.attendee.set(sources.attendee);
+				debugger;
+            }
+        });
+		
+		sources.answers.survey.set(sources.survey);
+		sources.answers.save();
+		$$('button1').hide();
+		$$('button2').hide();
 		console.log("answer saved");
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-
+		attendeeObj= {};
+		sources.attendeeObj.sync();
 	};// @lock
 
 // @region eventManager// @startlock
